ba2395f54196d27f311ab85079355b4ba587fe4d
gdc94 <germanderbescatoni@gmail.com>
2023-07-15 23:55:49 -0300

    card list section

---
+++ src/components/cardListSection/cardListSection.tsx
@@ -0,0 +1,10 @@
+import { FunctionComponent } from "react";
+import { CardListContainer } from "./cardListSection.styles";
+
+interface CardListSectionProps {}
+
+const CardListSection: FunctionComponent<CardListSectionProps> = ({}) => {
+  return <CardListContainer></CardListContainer>;
+};
+
+export default CardListSection;
